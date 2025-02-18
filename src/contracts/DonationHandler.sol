346b48f049403ea92ac5e1851156aa2d526fde7f
Mitch Oz <divine_comedian@protonmail.com>
2025-02-18 11:36:53 -0600

    update remappings and add contract starter

---
+++ src/contracts/DonationHandler.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import "@openzeppelin/contracts/interfaces/IERC20.sol";
+import "@openzeppelin/contracts-upgradeable/utils/ReentrancyGuardUpgradeable.sol";
+
+
+
+
+contract DonationHandler is OwnableUpgradeable, ReentrancyGuardUpgradeable {
+
+
+    
+}
