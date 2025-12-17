c8c841a9680a00f8f67fbf9c8686e2d9afe8da9d
Mitch Oz <ecp.ozz@gmail.com>
2025-12-17 10:24:21 -0600

    feat: add proxy admin addresses to deployment script

--- script/DeployDonationHandler.s.sol
+++ script/DeployDonationHandler.s.sol
@@ -8,6 +8,11 @@ import {Script} from 'forge-std/Script.sol';
 import {console} from 'forge-std/console.sol';
 
 contract DeployDonationHandler is Script {
+  address public constant ethereumProxyAdmin = 0x7cD4eAAed06fA2270e0C063B7aBDa576a0ad149F;
+  address public constant gnosisProxyAdmin = 0x076C250700D210e6cf8A27D1EB1Fd754FB487986;
+  address public constant optimismProxyAdmin = 0x2f2c819210191750F2E11F7CfC5664a0eB4fd5e6;
+  address public constant polygonProxyAdmin = 0x7a5D2A00a25b95fd8739bc52Cd79f8F971C37Ca1;
+
   function run() external {
     uint256 deployerPrivateKey = vm.envUint('PRIVATE_KEY');
 
@@ -15,7 +20,7 @@ contract DeployDonationHandler is Script {
 
     DonationHandler donationHandler = new DonationHandler();
 
-    ProxyAdmin proxyAdmin = new ProxyAdmin(address(this));
+    ProxyAdmin proxyAdmin = new ProxyAdmin(msg.sender);
     TransparentUpgradeableProxy proxy = new TransparentUpgradeableProxy(
       address(donationHandler), address(proxyAdmin), abi.encodeWithSelector(DonationHandler.initialize.selector)
     );
